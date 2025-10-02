408ff07e0fa45f17ce64c7d7511c6c842629e1cc
Saqamos <131597696+Saqamos@users.noreply.github.com>
2025-10-02 22:55:59 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+plugins { }
+buildscript {
+    repositories { google(); mavenCentral() }
+    dependencies { classpath("com.android.tools.build:gradle:8.6.0") }
 }
+allprojects { repositories { google(); mavenCentral() } }
+tasks.register("clean", Delete::class) { delete(rootProject.buildDir) }
